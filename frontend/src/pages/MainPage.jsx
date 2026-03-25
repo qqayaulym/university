e12b3a200a510ef24fae4c6dc2e54003bc00e7d1
ayaulym <qrwmum@gmail.com>
2026-03-26 02:21:24 +0600

    a: почти басты бет бітті, примерный шаблоны жасалды, стилін келтірдім

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom"
 import { Link } from "react-router-dom"
+import "../styles/mainpage.css"
 
 const MainPage = () => {
   const navigate = useNavigate();
@@ -11,9 +12,13 @@ const MainPage = () => {
 
   return (
     <div className="main">
-      <h2>Типа пр</h2>
+      <h2 className="mainH2">AITU студенттік өмірінің орталығына қош келдіңіз!</h2>
+      <p className="mainP">Мұнда сіз университет қабырғасында өтетін барлық қызықты іс-шараларды тауып, бірнеше секунд ішінде тіркеле аласыз. Кино кештерінен бастап, спорттық секцияларға дейін — бәрі осында!</p>
+      <img className="mainPhoto" src="none" alt="" />
+      <h2 className="mainH2">Жақында болатын іс-шаралар</h2>
+      <h2 className="mainH2">Апталық күнтізбе</h2>
       <button className="activitiesButton" onClick={() => navigate("/activities")}>Перейти к активам</button>
-      <button><a href="https://astanait.edu.kz/">Перейти на сайт айту</a></button>
+      <button className="aituButton"><a href="https://astanait.edu.kz/">Перейти на сайт айту</a></button>
     </div>
   );
 };
